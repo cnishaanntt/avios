056dafed44e520e399c0f691bdeac7b4157536d1
cnishaanntt <cnishaanntt@gmail.com>
2017-09-20 17:37:57 +0530

    Update xlsextension.js

--- www/js/xlsextension.js
+++ www/js/xlsextension.js
@@ -40,10 +40,20 @@ XLSExtension.prototype.load = function () {
     };
     button1.addClass('toolbarXLSButton');
     button1.setToolTip('Export to .XLSX');
+    
+    // Button 2
+    var button2 = new Autodesk.Viewing.UI.Button('toolbarXLS');
+    button1.onClick = function (e) {
+       status('just like it');
+    };
+    button2.addClass('toolbarXLSButton');
+    button2.setToolTip(':-)');
 
     // SubToolbar
     this.subToolbar = new Autodesk.Viewing.UI.ControlGroup('myAppGroup1');
     this.subToolbar.addControl(button1);
+    this.subToolbar.addControl(button2);
+    
 
     _viewer.toolbar.addControl(this.subToolbar);
   };
